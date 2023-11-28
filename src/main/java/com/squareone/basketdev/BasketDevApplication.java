d1feee47276b948a766becc58ae6e13c41a17397
squareOne073 <squareo073@gmail.com>
2023-11-28 23:51:32 +0900

    0

---
+++ src/main/java/com/squareone/basketdev/BasketDevApplication.java
@@ -0,0 +1,13 @@
+package com.squareone.basketdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketDevApplication.class, args);
+	}
+
+}
